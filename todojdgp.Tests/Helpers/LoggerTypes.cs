b02a3752301a343e2aca1eca49f1695e32972c0b
JUAN DIEGO GIL PATI�O <juangil257447@correo.itm.edu.co>
2021-09-05 22:44:27 -0500

    UNIT TEST PREPARATION

---
+++ todojdgp.Tests/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todojdgp.Tests.Helpers
+{
+    public enum LoggerTypes
+    {
+        Null,
+        List
+    }
+}
